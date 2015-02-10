a04887510e458b7e006a73edebdabd0b5370e9ab
Andreas Neuhauser <andreas.neuhauser@gmail.com>
2015-02-10 12:18:38 +0100

    Show values of sensors in kendo grid

--- app/controllers/sensorDetailController.js
+++ app/controllers/sensorDetailController.js
@@ -8,11 +8,29 @@ wotApp.controller('sensorDetailController', ['$scope', '$stateParams', 'SensorSe
         [28, 48, 40, 19, 86, 27, 90]
     ];
 
+    $scope.gridColumns = [
+        { field: "ticks", title: "Date" },
+        { field: "value", title: "Value" }
+    ];
+
     SensorService.getValues().then(function(res){
         var data = res.data;
+        $scope.gridData = new kendo.data.DataSource({
+            data: data,
+            schema:{
+                parse:function (response) {
+                    $.each(response, function (idx, elem) {
+                        if (elem.ticks && typeof elem.ticks === "string") {
+                            elem.ticks = moment(elem.ticks).format("MMM Do YY");
+                        }
+                    });
+                    return response;
+                }
+            },
+            pageSize: 10
+        });
         $scope.lastValue = data[0].value;
-        $scope.lastUpdated = moment(data[2].ticks).startOf("minutes").fromNow();
-        //$scope.lastUpdated = moment.unix(data[2].ticks).format("MMM Do YY");
+        $scope.lastUpdated = moment(data[0].ticks).startOf("minutes").fromNow();
     }, function(error){
         console.log('error during getSensors');
     });
